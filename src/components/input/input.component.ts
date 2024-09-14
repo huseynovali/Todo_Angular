84c4895850c8e72efa6151d67a4e2f0de15f94d7
User <huseynovali612@gmail.com>
2024-09-14 16:20:31 +0400

    config

--- src/components/input/input.component.ts
+++ src/components/input/input.component.ts
@@ -1,12 +1,23 @@
-import { Component } from '@angular/core';
+import { Component, EventEmitter, Output } from '@angular/core';
+import { FormControl, FormGroup, ReactiveFormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-input',
   standalone: true,
-  imports: [],
+  imports: [ReactiveFormsModule],
   templateUrl: './input.component.html',
-  styleUrl: './input.component.css'
+  styleUrl: './input.component.css',
 })
 export class InputComponent {
+  @Output() addTodo = new EventEmitter<any>();
+  i = 0;
+  profileForm = new FormGroup({
+    todo: new FormControl(''),
+  });
 
+  handleSubmit() {
+    this.i++;
+    this.addTodo.emit({ ...this.profileForm.value, id: this.i });
+    this.profileForm.value.todo = '';
+  }
 }
